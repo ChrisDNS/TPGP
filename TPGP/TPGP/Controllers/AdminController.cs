cabf50d09b5abff100d5b2eea0843b650196caae
camarasidi12@gmail.com <camarasidi12@gmail.com>
2018-01-04 12:14:02 +0100

    utilisation de UserRepository a la place de User

--- TPGP/TPGP/Controllers/AdminController.cs
+++ TPGP/TPGP/Controllers/AdminController.cs
@@ -4,6 +4,8 @@ using TPGP.Models.Jobs;
 using TPGP.Context;
 using System.Collections.Generic;
 using System.Linq;
+using TPGP.DAL.Repositories;
+using TPGP.DAL.Interfaces;
 
 namespace TPGP.Controllers
 {
@@ -11,24 +13,27 @@ namespace TPGP.Controllers
     public class AdminController : Controller
     {
         TPGPContext context;
-        public AdminController(/*[FromService] TPGPContext context*/)
+        IUserRepository userrepository;
+        public AdminController(IUserRepository usr)
         { // context.users.Skip(4).take(5).tolist() // saut de users
             // ff.Include(p=>p.cate) inclure les lazy
             context = new TPGPContext();
+            userrepository = usr;
         }
         // GET: Admin
         public ActionResult Index()
         {
-            IEnumerable<User> us = context.Users.ToList();
+            
+            IEnumerable<User> us = userrepository.GetAll();
             return View("AdminView", us);
         }
 
-        public ActionResult Edit(int id)
+        public ActionResult Edit(long id)
         {
-           
-            User us = context.Users
+            var us = userrepository.GetById(id);
+           /* User us = context.Users
                 .Where(p => p.Id.Equals(id))
-                .FirstOrDefault<User>();
+                .FirstOrDefault<User>();*/
 
             return View("edit",us);
         }
@@ -36,15 +41,17 @@ namespace TPGP.Controllers
         public ActionResult Save(User user)
         {
 
-            User us = context.Users
-                .Where(p => p.Username.Equals(user.Username))
+            var us = userrepository.GetAll().Where(p => p.Username.Equals(user.Username))
                 .FirstOrDefault<User>();
+           /* User us = context.Users
+                .Where(p => p.Username.Equals(user.Username))
+                .FirstOrDefault<User>();*/
             if (us != null)
             {
                 us.Username = user.Username;
                 us.Role.RoleName = user.Role.RoleName;
             }
-            context.SaveChanges();
+            userrepository.SaveChanges();
             IEnumerable<User> users = context.Users.ToList();
             
             return View("AdminView", users);
